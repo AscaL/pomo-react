ac14791c8cf1b7e99e5b967a3c0d95eda161a5ca
AscaL <emanueleorfanelli@gmail.com>
2017-04-09 16:28:13 +0200

    update yarn and timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -16,7 +16,7 @@ function displayTime(timeInSeconds) {
   if (timeInSeconds % 60 === 0) {
     return timeInSeconds / 60;
   } else {
-    const minutes = timeInSeconds / 60;
+    const minutes = Math.floor(timeInSeconds / 60);
     console.log('minutes', minutes);
     const seconds = timeInSeconds % 60;
     console.log('seconds', seconds);
@@ -28,7 +28,9 @@ function TimerCounter({ props }) {
   // need better way to display
   console.log('TimerCounter props: ', props);
   const minutesInSeconds = (props.minutes - 1) * 60;
+  console.log('minutesInSeconds', minutesInSeconds);
   const timeInSeconds = minutesInSeconds + props.seconds;
+  console.log('timeInSeconds', timeInSeconds);
   return (
     <p>
       {displayTime(timeInSeconds)}
